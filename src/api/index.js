41c3c3d94170a9eec2bb177068edd97ecefdf826
SahTitus <sahtitus58@gmail.com>
2022-08-10 05:26:17 +1200

    commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,19 +1,20 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL:"https://hookengine.vercel.app" })
+const API = axios.create({ baseURL: "https://hook-murex.vercel.app" });
 // const linkUrl = "https://hooklearn.herokuapp.com/link"
 // const url = "https://hooklearn.herokuapp.com";
 // const url = "https://hookengine.vercel.app";
 // const url = "http://localhost:5000"
 
 API.interceptors.request.use((req) => {
-    if (localStorage.getItem('profile')) {
-        req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
-    }
+  if (localStorage.getItem("profile")) {
+    req.headers.Authorization = `Bearer ${
+      JSON.parse(localStorage.getItem("profile")).token
+    }`;
+  }
 
-    return req;
-})
-
+  return req;
+});
 
 export const fetchPosts = (page) => API.get(`/posts`);
 export const fetchPost = (id) => API.get(`/posts/${id}`);
@@ -21,9 +22,9 @@ export const detectLink = (link) => API.post(`/link}`, link);
 export const comment = (id, comment) => API.post(`/${id}`, comment);
 export const createPost = (newPost) => API.post(`/posts`, newPost);
 export const deletePost = (id) => API.delete(`/posts/${id}`);
-export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
+export const updatePost = (id, updatedPost) =>
+  API.patch(`/posts/${id}`, updatedPost);
 export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
 
 export const signIn = (formData) => API.post(`/user/signin`, formData);
 export const signUp = (formData) => API.post(`/user/signup`, formData);
- 

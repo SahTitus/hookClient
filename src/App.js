f9ffcea5366220c84440c02159d2e291030a3e17
SahTitus <sahtitus58@gmail.com>
2022-08-10 07:46:28 +1200

    bom

--- src/App.js
+++ src/App.js
@@ -1,50 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
 import "./styles/App.css";
-// import GroupPage from "./pages/GroupPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomeFeed from "./pages/HomePage";
-// import Myhalls from "./pages/Myhalls";
 import Videos from "./pages/Questions";
 import Chats from "./pages/Chats";
 import ChatRoom from "./pages/ChatRoom";
-// import PostForm from "./pages/PostForm";
 import PostDetails from "./pages/PostDetails";
-// import CreateGroup from "./pages/CreateGroup";
 import Profile from "./pages/Profile";
-// import PollLeaf from "./PollLeaf";
-// import Explore from "./pages/Explore";
-// import PollComponent from "./PollComponent";
 import Auth from "./components/auth/Auth";
 import SearchPage from "./pages/Search";
-// import Halls from "./pages/Halls";
 import AddPost from "./components/Post/AddPost";
 import {useTheme }from "./utils/useTheme";
 import { ThemeProvider } from "styled-components";
 import  ThemeStyle  from "./styles/ThemeStyle";
 import useNetwork from './utils/useNetwork'
-import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
-import { fetchPosts } from "./actions/posts";
 import OfflineMsg from "./components/connectivity/OfflineMsg"
-// import { authData } from "./redux/auth";
-
-
 
 function App() {
   const theme = useTheme();
-  const dispatch = useDispatch();
   const { online } = useNetwork();
-  const [refresh, setRefresh] = useState(false);
-  const { posts, isLoading, error} = useSelector((state) => state.posts);
-  // const {user } = useSelector((state) => state.auth);
-  const feeds = posts.slice().sort((a, b) => b.createdAt.localeCompare(a.createdAt))
-
-// console.log(user)
-
-  useEffect(() => {
-    dispatch(fetchPosts());
-  }, [dispatch]);
-
 
   return (
     <ThemeProvider theme={theme}>

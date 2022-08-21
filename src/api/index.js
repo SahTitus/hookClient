0a24e18ca1d651d1073b03f2e8f19c51af3aa9c5
SahTitus <sahtitus58@gmail.com>
2022-08-22 01:21:39 +1200

    updates

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://hookengine.vercel.app" });
 // const linkUrl = "https://hooklearn.herokuapp.com/link"
 // const url = "https://hooklearn.herokuapp.com";
 // const url = "https://hookengine.vercel.app";
